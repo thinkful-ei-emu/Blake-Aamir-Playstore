28f934703fcab538a83a3804c399105c10601153
Blake Lowrey <blake.lowrey1@gmail.com>
2019-07-16 15:25:00 -0400

    final

--- index.js
+++ index.js
@@ -14,13 +14,36 @@ app.get('/sort', (req, res)=>{
   }
   if(!(genre === 'Action' || genre === 'Puzzle' || genre === 'Strategy' || genre ==='Casual' || genre ==='Arcade' || genre === 'Card' )){
     return res.status(400)
-    .send('Genre value in invalid');
+      .send('Genre value in invalid');
   }
 
+  const workingArray = playstore;
+  if (sort === 'rating'){
+    workingArray.sort((a,b) => {
+      return b.Rating - a.Rating;
+    });
+  }
+  if (sort === 'app') {
+    workingArray.sort((a,b) => {
+      let aApp = a.App;
+      let bApp = b.App;
+      let aCharCode = aApp.charCodeAt(0);
+      let bCharCode = bApp.charCodeAt(0);
+      return aCharCode - bCharCode;
+    });
+  }
+  const finalArray = workingArray.filter(item => {
+    
+    if (item.Genres.includes(genre)){
+      console.log(item.Genres + ' includes: ' + genre);
+      return true;
+    }
+    else{
+      return false;
+    }
+  });
 
-
-
-  return res.send('it was valid');
+  return res.json(finalArray);
 
 
 });
